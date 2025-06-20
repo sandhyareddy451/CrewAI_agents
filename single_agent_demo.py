fb28936d4b3c9d794d316e161121202deabac35d
sandhyareddy451 <vemula.sandhya51@gmail.com>
2025-06-20 14:43:26 +0530

    agent_demo

--- single_agent_demo.py
+++ single_agent_demo.py
@@ -7,18 +7,44 @@ from langchain_openai import ChatOpenAI
 load_dotenv()
 
 SERPER_API_KEY = os.getenv("SERPER_API_LEY")
-OPENAI_API_KEY = os.getenv("OPENAI_API_LEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 search_tool = SerperDevTool()
+llm = ChatOpenAI("gpt-3.5-turbo")
 
 
 def create_research_agent():
     return Agent(
-        role = "Reasearch_Agent"
-        
-        
-        
-        
+        role = "Reasearch_Specialist",
+        gole = "conduct through research on given topic",
+        backstory = "you are an experienced researcher with expertise in finding and synthesizing information from various sources",
+        verbose = True,
+        allow_deligation = False,
+        tools = [search_tool],
+        llm = llm
         
         
     )
+
+def create_task(agent, topic):
+    return Task(
+        description =f"Research on the following topic and provide a comprehensive summary : {topic}", 
+        agent = agent,
+        expected_output = "A detailed summery of the research findings, including key insights "
+                
+
+    )
+    
+def run_research(topic):
+    
+    agent = create_research_agent()
+    task = create_task(agent, topic)
+    crew = Crew(agent = [agent], task = [task])
+    results = crew.kickoff()
+    return results
+
+if __name__== "__main__":
+    print ("welcome to the research agents ")
+    topic = input("enter the research topic")
+    result = run_research(topic)
+    print("research results ")
